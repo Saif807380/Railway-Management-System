ee7ec18be4b5c2a2d6833c6e83c7a2692f0d0ffc
Saif Kazi <saif1204kazi@gmail.com>
2020-03-17 18:35:45 +0530

    Added new tables

--- train/models.py
+++ train/models.py
@@ -19,6 +19,48 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
+	passengers = db.relationship('Passenger', backref='passengers', lazy=True)
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}')"
+
+class Passenger(db.Model):
+	pass_id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(20),nullable=False)
+	age = db.Column(db.Integer,nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+	ticket = db.relationship('Ticket',backref='ticket',lazy=True,uselist=False)  #uselist=False implies one-to-one relationship
+	def __repr__(self):
+		return f"Passenger('{self.pass_id}','{self.name}','{self.age}','{self.user_id}')"
+
+class Ticket(self):
+	pnr_number = db.Column(db.String(10),primary_key=True)
+	destination = db.Column(db.String(20),nullable=False)
+	source = db.Column(db.String(20),nullable=False)
+	journey_date = db.Column(db.DateTime,nullable=False)
+	seat_no = db.Column(db.Integer,unique=True,nullable=False)
+	pass_id = db.Column(db.Integer,db.ForeignKey('passenger.pass_id'),nullable=False)
+	train_no = db.Column(db.Integer,db.ForeignKey('train.train_no'),nullable=False)
+
+	def __repr__(self):
+		return f"Ticket('{self.pnr_number}', '{self.source}', '{self.destination}', '{self.journey_date}', '{self.train_no}', '{self.seat_no}', '{self.pass_id}')"
+
+class Train(self):
+	train_no = db.Column(db.Integer,primary_key=True)
+	train_name = db.Column(db.String(30),unique=True,nullable=False)
+	source_id = db.Column(db.Integer,db.ForeignKey('station.station_id'),nullable=False)
+	destination_id = db.Column(db.Integer,db.ForeignKey('station.station_id'),nullable=False)
+
+	def __repr__(self):
+		return f"Train('{self.train_no}', '{self.train_name}')"
+
+class Station(self):
+	station_id = db.Column(db.Integer,primary_key=True)
+	station_name = db.Column(db.String(20),nullable=False,unique=True)
+	train_source = db.relationship('Train',backref='starting_trains',lazy=True)
+	train_destination = db.relationship('Train',backref='ending_trains',lazy=True)
+
+	def __repr__(self):
+		return f"Station('{self.station_id}', '{self.station_name}')"
+
+
