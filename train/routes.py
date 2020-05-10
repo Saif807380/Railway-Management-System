3e1fea0ced189762909d3a6f20ea4c5a96325b77
Saif Kazi <saif1204kazi@gmail.com>
2020-05-10 17:40:20 +0530

    Minor bug fix

--- train/routes.py
+++ train/routes.py
@@ -20,6 +20,7 @@ def is_time_between(begin_time, end_time, check_time=None):
 
 @app.route('/home')
 def home():
+	session["adminLog"] = 0
 	return render_template('index.html',admin = session["adminLog"])
 
 @app.route('/book_ticket',methods=['GET','POST'])
